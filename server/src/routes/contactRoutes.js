c3d926c5bf70b7b8ca687199f65b27a376aadd1b
YASH RAJ <99674512+Y4shr4j@users.noreply.github.com>
2024-05-27 00:09:32 +0530

    Delete server directory

--- server/src/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createContact, getAllContacts } from '../controllers/contactController.js';
-
-const router = express.Router();
-
-router.post('/', createContact);
-router.get('/', getAllContacts); // Route to get all contacts
-
-export default router;
